dd7f5247d56b584112a06cd2f3ecd0cd58673c34
jdjhaha <jdjhaha1@gmail.com>
2021-08-27 17:57:50 +0900

    핸드폰에서 보려고 cors 아이피 추가

--- src/main/java/com/jdjhaha/vocab/login/controller/LoginController.java
+++ src/main/java/com/jdjhaha/vocab/login/controller/LoginController.java
@@ -31,7 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "http://192.168.44.120:3000"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://192.168.215.54:3000"})
 public class LoginController {
 	@Autowired
     private AuthenticationManager authenticationManager;
