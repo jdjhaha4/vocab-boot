387627a83664ee335910b3565fd0cc61c0d07e60
jdjhaha <jdjhaha1@gmail.com>
2021-10-22 17:55:26 +0900

    틀리거나, 모름 선택한 단어 테이블 추가중..

--- src/main/java/com/jdjhaha/vocab/vocab/mapper/VocabAttentionMappingMapper.java
+++ src/main/java/com/jdjhaha/vocab/vocab/mapper/VocabAttentionMappingMapper.java
@@ -1,19 +1,12 @@
 package com.jdjhaha.vocab.vocab.mapper;
 
 import java.util.HashMap;
-import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import com.jdjhaha.vocab.vocab.vo.VocabVO;
-
 @Repository
 @Mapper
 public interface VocabAttentionMappingMapper {
-	List<HashMap<Object, Object>> selectData(HashMap<Object, Object> paramMap);
-	
-	int insertData2(VocabVO vocab);
 	int insertData(HashMap<Object, Object> paramMap);
-	int deleteData(HashMap<Object, Object> map);
 }
